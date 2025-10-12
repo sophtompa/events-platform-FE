3f280e87306ad64da57b514e666a1095dc857b1f
Sophie <sophtompa@gmail.com>
2025-10-12 14:05:44 +0100

    add users component

--- events-platform/components/Users.jsx
+++ events-platform/components/Users.jsx
@@ -1,9 +1,37 @@
+import { useState, useEffect } from "react";
+import { getAllUsers } from "../api";
+
 function Users() {
+    const [isLoading, setLoading] = useState(true)
+    const [users, setUsers] = useState([])
+
+    useEffect(
+        () => { 
+            getAllUsers().then((userData) => { 
+                setUsers(userData)
+                setLoading(false)
+            })
+        }, [])
+
+        if (isLoading) return <h3>Getting Events...</h3>
+
 
     return (
+        <>
         <header>
             <h1>Users</h1>
         </header>
+        <section>
+        {users.map((user) => {
+            return (
+                <ul key={user.username} className='user-display' value={user.id}>
+                    <li className='user'>{user.username}</li>
+                </ul>
+            )
+        })}
+        </section>
+        
+        </>
     )
 }
 
