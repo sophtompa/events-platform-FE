ec503380480f719ea75a3b5de593381ef7260335
Sophie <sophtompa@gmail.com>
2025-10-18 18:48:52 +0100

    add delete account button

---
+++ events-platform/components/DeleteUser.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { deleteUserByUsername } from "../api";
+
+function DeleteUser() {
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const username = localStorage.getItem("loggedInUsername");
+
+    function handleDelete() {
+
+        if (!window.confirm("Are you sure you want to delete your account?")) return;
+        setLoading(true);
+
+        deleteUserByUsername(username)
+            .then(() => {
+                localStorage.removeItem("token");
+                localStorage.removeItem("loggedInUsername");
+                alert("Your account has been deleted.");
+                navigate("/");
+            })
+            .catch((err) => {
+                console.error("Error deleting user:", err);
+                alert("Failed to delete account. Check console for details.");
+            })
+            .finally(() => setLoading(false));
+    
+
+}
+
+    return(
+        <button className='delete-button' onClick={handleDelete} disabled={loading}> {loading ? "Deleting..." : "Delete Account"}</button>
+    )
+}
+
+export default DeleteUser;
