f1a1684f3394861613a4dd602a2470938c9244fa
Sophie <sophtompa@gmail.com>
2025-10-19 15:24:44 +0100

    small fix

--- events-platform/components/StaffNav.jsx
+++ events-platform/components/StaffNav.jsx
@@ -4,7 +4,7 @@ function StaffNav() {
 
     return (
         <nav>
-            <Link to='/'>Back to Home</Link>
+            <Link to='/'>Home</Link>
             <Link to='/events'>Events</Link>
             <Link to='/users'>Users</Link>
             <Link to='/staff-user'>My Account</Link>
